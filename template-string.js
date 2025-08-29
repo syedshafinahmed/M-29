efe56887e3def5f903f89358661b05b26e84b432
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-29 23:59:26 +0600

    added

---
+++ template-string.js
@@ -0,0 +1,25 @@
+// const poem = "cnslvfnl \n" + "cioasieisefj \n";
+// console.log(poem);
+
+
+const poem2 = `cdebneilfjoa
+efrmjgeo;prgj
+rfregniejgtjh
+fnriofnwncij`
+console.log(poem2);
+
+
+// function sum(num1, num2){
+//     const result = num1 + num2;
+//     const output = 'sum of ' + num1 + ' and ' + num2 + ' is equal to ' + result;
+//     console.log(output);
+// }
+// sum(10,30)
+
+
+function sum(num1, num2){
+    const result = num1 + num2;
+    const output = `sum of ${num1} and ${num2} is ${result}`
+    console.log(output);
+}
+sum(10,30)
