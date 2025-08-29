a45f341c7d77bac77eaf4b979ef780db288fe9b7
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-08-29 23:41:40 +0600

    added

---
+++ default-parameter.js
@@ -0,0 +1,10 @@
+function sum(num1, num2=100){
+    const total = num1 + num2;
+    console.log(num1, num2, total);
+}
+sum(10);
+
+// thumb rule of default value
+// string --> ''
+// array --> []
+// object --> {}
